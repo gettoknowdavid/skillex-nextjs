7e959ff9e06c9af2b035593fb463dd10f840cad4
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 21:46:53 +0100

    feat: create baseweb theme file

---
+++ lib/theme.tsx
@@ -0,0 +1,8 @@
+import { createLightTheme, lightThemePrimitives } from 'baseui';
+
+const SkillexTheme = createLightTheme({
+  ...lightThemePrimitives,
+  primaryFontFamily: '"Archia", sans-serif',
+}, {});
+
+export default SkillexTheme;
