0a5bba4706e26338d822dde1f575062ff0660c90
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 23:23:15 +0100

    feat: add action buttons to header

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button, SHAPE, SIZE } from 'baseui/button';
 
 function Header() {
   return (
@@ -8,20 +9,28 @@ function Header() {
       <div>
         <Image src="/logo.svg" alt="Skillex Logo" width="124px" height="36px" />
       </div>
-      <ul>
-        <li>
-          <Link href="/"><a>Find passion</a></Link>
-        </li>
-        <li>
-          <Link href="/"><a>Categories</a></Link>
-        </li>
-        <li>
-          <Link href="/"><a>Skills</a></Link>
-        </li>
-        <li>
-          <Link href="/"><a>Customer</a></Link>
-        </li>
-      </ul>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/"><a>Find passion</a></Link>
+            </li>
+            <li>
+              <Link href="/"><a>Categories</a></Link>
+            </li>
+            <li>
+              <Link href="/"><a>Skills</a></Link>
+            </li>
+            <li>
+              <Link href="/"><a>Customer</a></Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <Link href="/"><a>Login</a></Link>
+        <Button size={SIZE.compact} shape={SHAPE.pill}>Free Trial</Button>
+      </div>
     </header>
   );
 }
