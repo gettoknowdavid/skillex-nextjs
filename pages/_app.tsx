ad0751dca3e5c9d723f089214e2ca874405d7e2a
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 23:07:34 +0100

    feat: add persistent layout to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { ReactElement, ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 import { BaseProvider } from 'baseui';
 import { Provider as StyletronProvider } from 'styletron-react';
+import { NextPage } from 'next';
 import SkillexTheme from '../lib/theme';
 import styletron from '../lib/styletron';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
+type NextPageWithLayout = NextPage & {
+    // eslint-disable-next-line no-unused-vars
+    getLayout?: (page: ReactElement) => ReactNode;
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+
   return (
     <StyletronProvider value={styletron}>
       <BaseProvider theme={SkillexTheme}>
-        <Component {...pageProps} />
+        {getLayout(<Component {...pageProps} />)}
       </BaseProvider>
     </StyletronProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import type { NextPage } from 'next';
 import { HeadingLarge } from 'baseui/typography';
+import Layout from '../components/layout';
 
 const Home: NextPage = function () {
   return (
     <div>
       <HeadingLarge>Welcome Home</HeadingLarge>
     </div>
+  );
+};
+
+// @ts-ignore
+Home.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <Layout>{page}</Layout>
   );
 };
 
