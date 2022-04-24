ebffc9df0d96f5ee31ebf2c3660153d172e8ec52
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 22:39:46 +0100

    updates

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,8 @@ import type { AppProps } from 'next/app';
 import { BaseProvider } from 'baseui';
 import { Provider as StyletronProvider } from 'styletron-react';
 import SkillexTheme from '../lib/theme';
-import '../styles/globals.css';
 import styletron from '../lib/styletron';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
