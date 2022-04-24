ead78e2362e4a1a1357bba8444a12e5e6aa9b467
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 23:14:46 +0100

    feat: add header to layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../organisms/header';
 
 type LayoutProps = {
     children: React.ReactNode
@@ -7,7 +8,7 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <>
-      <header>Skillex.</header>
+      <Header />
       <main>
         {children}
       </main>
