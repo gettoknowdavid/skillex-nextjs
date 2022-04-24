f58ff3f076c7b5e9cdcee4c47756751097356a6d
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 23:18:36 +0100

    feat: add nav list to header
    uninstall react-router-dom

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 function Header() {
   return (
@@ -7,6 +8,20 @@ function Header() {
       <div>
         <Image src="/logo.svg" alt="Skillex Logo" width="124px" height="36px" />
       </div>
+      <ul>
+        <li>
+          <Link href="/"><a>Find passion</a></Link>
+        </li>
+        <li>
+          <Link href="/"><a>Categories</a></Link>
+        </li>
+        <li>
+          <Link href="/"><a>Skills</a></Link>
+        </li>
+        <li>
+          <Link href="/"><a>Customer</a></Link>
+        </li>
+      </ul>
     </header>
   );
 }
