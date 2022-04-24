3633c9936d97a2c49824a3df7cacbb72adf0b7cc
David Michael II <knowdavidmichael@gmail.com>
2022-04-24 21:31:58 +0100

    feat: add custom _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/no-array-index-key */
+import * as React from 'react';
+import Document, {
+  Head, Html, Main, NextScript,
+} from 'next/document';
+import { Server, Sheet } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import styletron from '../styletron';
+
+// @ts-ignore
+class MyDocument extends Document <{ stylesheets: Sheet[] }> {
+  static getInitialProps = async (ctx: any) => {
+    const renderPage = () => ctx.renderPage({
+      // eslint-disable-next-line func-names
+      enhanceApp: (App: any) => function (props: any) {
+        return (
+          <StyletronProvider value={styletron}>
+            <App {...props} />
+          </StyletronProvider>
+        );
+      },
+    });
+
+    const stylesheets = (styletron as Server).getStylesheets() || [];
+    return { ...renderPage, stylesheets };
+  };
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {this.props.stylesheets.map((sheet, index) => (
+            <style
+              className="_styletron_hydrate_"
+              dangerouslySetInnerHTML={{ __html: sheet.css }}
+              media={sheet.attrs.media}
+              data-hydrate={sheet.attrs['data-hydrate']}
+              key={index}
+            />
+          ))}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
