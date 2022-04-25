6aa0474d684482dea1b13603ca8f90cec2a113df
David Michael II <knowdavidmichael@gmail.com>
2022-04-25 13:57:40 +0100

    feat: create drawer component

---
+++ components/organisms/drawer/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ANCHOR, Drawer as BaseDrawer } from 'baseui/drawer';
+import GlobalContext from '../../../contexts/global.context';
+
+function Drawer() {
+  const { drawerOpen, closeDrawer } = React.useContext(GlobalContext);
+
+  return (
+    <BaseDrawer
+      isOpen={drawerOpen}
+      onClose={closeDrawer}
+      autoFocus
+      anchor={ANCHOR.top}
+      overrides={{
+        Root: { style: () => ({ zIndex: 80 }) },
+        DrawerBody: { style: () => ({ display: 'flex', justifyContent: 'center' }) },
+        DrawerContainer: { style: () => ({ display: 'flex', alignItems: 'center' }) },
+      }}
+    >
+      s
+    </BaseDrawer>
+  );
+}
+
+export default Drawer;
