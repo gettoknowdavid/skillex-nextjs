3af6473ea417859dac99458407256ae8d176f0d2
David Michael II <knowdavidmichael@gmail.com>
2022-04-25 13:54:41 +0100

    feat: add global context provider to layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import Header from '../organisms/header';
+import GlobalContext from '../../contexts/global.context';
 
 type LayoutProps = {
     children: React.ReactNode
 }
 
 function Layout({ children }: LayoutProps) {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const toggleDrawer = React.useCallback(() => {
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  const globalValue = React.useMemo(() => ({
+    drawerOpen: isOpen,
+    toggleDrawer,
+    closeDrawer: () => setIsOpen(false),
+  }), [isOpen, toggleDrawer]);
+
   return (
-    <>
+    <GlobalContext.Provider value={globalValue}>
       <Header />
       <main>
         {children}
       </main>
-    </>
+    </GlobalContext.Provider>
   );
 }
 
