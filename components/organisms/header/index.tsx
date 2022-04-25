2a9832993ef9e12ef2d6ad15d82b180f49739b64
David Michael II <knowdavidmichael@gmail.com>
2022-04-25 13:42:28 +0100

    refactor: hamburger icon

--- components/organisms/header/index.tsx
+++ components/organisms/header/index.tsx
@@ -4,17 +4,10 @@ import Link from 'next/link';
 import { Button, SHAPE, SIZE } from 'baseui/button';
 import { useStyletron } from 'baseui';
 import { gsap } from 'gsap';
+import Hamburger from '../../atoms/hamburger';
 
 function Header() {
   const [css, theme] = useStyletron();
-  const [menuTl] = React.useState(gsap.timeline({ paused: true }));
-
-  React.useEffect(() => {
-    menuTl.to('.line-one', { duration: 0.1, y: 0, rotation: 45 })
-      .to('.line-two', { duration: 0.1, y: 0, rotation: -45 }, 0).reverse();
-  }, [menuTl]);
-
-  const onclick = () => menuTl.reversed(!menuTl.reversed());
 
   const linkStyles = css({
     display: 'inline-block',
@@ -146,53 +139,7 @@ function Header() {
             Free Trial
           </Button>
         </div>
-        <div
-          onClick={onclick}
-          role="button"
-          aria-label="Menu Button"
-          tabIndex={0}
-          onKeyDown={onclick}
-          className={css({
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginLeft: '24px',
-            position: 'relative',
-            width: '32px',
-            height: '32px',
-            padding: 0,
-            backgroundColor: theme.colors.primaryA,
-            transitionProperty: 'all',
-            transitionDuration: '.2s',
-            cursor: 'pointer',
-            [theme.mediaQuery.small]: { display: 'flex' },
-            [theme.mediaQuery.medium]: { display: 'flex' },
-            [theme.mediaQuery.large]: { display: 'none' },
-          })}
-        >
-          <div
-            className="line-one"
-            style={{
-              transform: 'translate3d(0px, -3px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-              transformStyle: 'preserve-3d',
-              position: 'absolute',
-              width: '16px',
-              height: '2px',
-              backgroundColor: theme.colors.mono100,
-            }}
-          />
-          <div
-            className="line-two"
-            style={{
-              transform: 'translate3d(0px, 3px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
-              transformStyle: 'preserve-3d',
-              position: 'absolute',
-              width: '16px',
-              height: '2px',
-              backgroundColor: '#fff',
-            }}
-          />
-        </div>
+        <Hamburger />
       </div>
     </header>
   );
