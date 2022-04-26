9593db8d912da0894df3c9df799b414ae40976a2
David Michael II <knowdavidmichael@gmail.com>
2022-04-26 23:56:34 +0100

    feat: add padding to section component

--- components/atoms/section/index.tsx
+++ components/atoms/section/index.tsx
@@ -2,39 +2,52 @@ import React from 'react';
 import { useStyletron } from 'baseui';
 
 type SectionType = {
+    id: string,
     children: React.ReactNode,
-    paddingTop: string[],
-    paddingBottom: string[],
+    paddingTop?: any,
+    paddingBottom?: any,
 };
 
-function Section({ children, paddingBottom, paddingTop }: SectionType) {
+function Section(props: SectionType) {
+  const {
+    id, children, paddingBottom, paddingTop,
+  } = props;
+
   const [css, theme] = useStyletron();
 
   return (
-    <section className={css({
-      [theme.mediaQuery.small]: {
-        paddingTop: paddingTop[0],
-        paddingRight: '24px',
-        paddingBottom: paddingBottom[0],
-        paddingLeft: '24px',
-      },
-      [theme.mediaQuery.medium]: {
-        paddingTop: paddingTop[1],
-        paddingRight: '1.714rem',
-        paddingBottom: paddingBottom[1],
-        paddingLeft: '1.714rem',
-      },
-      [theme.mediaQuery.large]: {
-        paddingTop: paddingTop[2],
-        paddingRight: '5rem',
-        paddingBottom: paddingBottom[2],
-        paddingLeft: '5rem',
-      },
-    })}
+    <section
+      id={id}
+      className={css({
+        minHeight: '100vh',
+        [theme.mediaQuery.small]: {
+          paddingTop: paddingTop.length !== 0 ? paddingTop[0] : paddingTop,
+          paddingRight: '24px',
+          paddingBottom: paddingBottom?.length !== 0 ? paddingBottom[0] : paddingBottom,
+          paddingLeft: '24px',
+        },
+        [theme.mediaQuery.medium]: {
+          paddingTop: paddingTop.length !== 0 ? paddingTop[1] : paddingTop,
+          paddingRight: '1.714rem',
+          paddingBottom: paddingBottom?.length !== 0 ? paddingBottom[1] : paddingBottom,
+          paddingLeft: '1.714rem',
+        },
+        [theme.mediaQuery.large]: {
+          paddingTop: paddingTop.length !== 0 ? paddingTop[2] : paddingTop,
+          paddingRight: '5rem',
+          paddingBottom: paddingBottom?.length !== 0 ? paddingBottom[2] : paddingBottom,
+          paddingLeft: '5rem',
+        },
+      })}
     >
       {children}
     </section>
   );
 }
+
+Section.defaultProps = {
+  paddingTop: [] || '',
+  paddingBottom: [] || '',
+};
 
 export default Section;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@ import React, { ReactElement } from 'react';
 import type { NextPage } from 'next';
 import { HeadingXXLarge } from 'baseui/typography';
 import Layout from '../components/layout';
+import Section from '../components/atoms/section';
 
 const Home: NextPage = function () {
   return (
     <>
-      <section id="find-position" style={{ paddingTop: '120px', minHeight: '100vh' }}><HeadingXXLarge>Find Position</HeadingXXLarge></section>
+      <Section id="find-position" paddingTop={['12px', '300px', '120px']}>
+        <HeadingXXLarge>Find Position</HeadingXXLarge>
+      </Section>
       <section id="categories" style={{ minHeight: '100vh' }}><HeadingXXLarge>Categories</HeadingXXLarge></section>
       <section id="skills" style={{ minHeight: '100vh' }}><HeadingXXLarge>Skills</HeadingXXLarge></section>
       <section id="customer" style={{ minHeight: '100vh' }}><HeadingXXLarge>Customer</HeadingXXLarge></section>
