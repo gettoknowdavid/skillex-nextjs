a30569dccb622ba099c2fc14e38186c5b7ce5fdd
David Michael II <knowdavidmichael@gmail.com>
2022-04-26 21:44:29 +0100

    fix: close drawer on nav link click

--- components/molecules/nav-list/index.tsx
+++ components/molecules/nav-list/index.tsx
@@ -3,21 +3,26 @@ import { gsap } from 'gsap/dist/gsap';
 import { ScrollToPlugin } from 'gsap/dist/ScrollToPlugin';
 import { StyledNav, StyledNavLink } from './styled-components';
 import LINKS, { NavLinkType } from '../../../lib/links';
+import GlobalContext from '../../../contexts/global.context';
 
 gsap.registerPlugin(ScrollToPlugin);
 
 function NavList() {
+  const { closeDrawer } = React.useContext(GlobalContext);
+
+  const onClick = (url: string) => {
+    closeDrawer();
+    gsap.to(window, {
+      duration: 1.6,
+      ease: 'power3.out',
+      scrollTo: { y: url, offsetY: 120 },
+    });
+  };
+
   return (
     <StyledNav>
       {LINKS.map((link: NavLinkType) => (
-        <StyledNavLink
-          key={link.id}
-          onClick={() => gsap.to(window, {
-            duration: 1.6,
-            ease: 'power3.out',
-            scrollTo: { y: link.url, offsetY: 120 },
-          })}
-        >
+        <StyledNavLink key={link.id} onClick={() => onClick(link.url)}>
           {link.name}
         </StyledNavLink>
       ))}
