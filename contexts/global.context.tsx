2b5260d5160c2b38dbf18a14efb29faed19079e6
David Michael II <knowdavidmichael@gmail.com>
2022-04-29 23:18:22 +0100

    feat: add GlobalType to GlobalContext

--- contexts/global.context.tsx
+++ contexts/global.context.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NavLinkType } from '../lib/links';
 
-const GlobalContext = React.createContext({
+type GlobalType = {
+  drawerOpen: boolean,
+  toggleDrawer: () => void,
+  closeDrawer: () => void,
+  links: NavLinkType[],
+}
+
+const GlobalContext: React.Context<GlobalType> = React.createContext<GlobalType>({
   drawerOpen: false,
   toggleDrawer: () => {},
   closeDrawer: () => {},
+  links: [],
 });
 
 export default GlobalContext;
