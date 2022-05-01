3c2c01aba543d44e230309008fd02d48af38ffb4
David Michael II <knowdavidmichael@gmail.com>
2022-05-01 23:40:27 +0100

    feat: updates to nav list

--- components/molecules/nav-list/index.tsx
+++ components/molecules/nav-list/index.tsx
@@ -7,33 +7,35 @@ import GlobalContext from '../../../contexts/global.context';
 gsap.registerPlugin(ScrollToPlugin);
 
 function NavList() {
+  const { closeDrawer, links } = React.useContext(GlobalContext);
   const [id, setId] = React.useState('#find-position');
   const timeline = gsap.timeline();
 
+  const updateNavLink = ({ url }: { url: string }) => {
+    timeline.addLabel(url).to(window, {
+      duration: 1.6,
+      ease: 'power3.out',
+      scrollTo: { y: url, offsetY: 80 },
+    });
+  };
+
   return (
-    <GlobalContext.Consumer>
-      {({ closeDrawer, links }) => (
-        <StyledNav>
-          {links.map((link) => (
-            <StyledNavLink
-              key={link.id}
-              $isActive={link.url === id}
-              onClick={() => {
-                closeDrawer();
-                setId(link.url);
-                timeline.to(window, {
-                  duration: 1.6,
-                  ease: 'power3.out',
-                  scrollTo: { y: link.url, offsetY: 80 },
-                });
-              }}
-            >
-              {link.name}
-            </StyledNavLink>
-          ))}
-        </StyledNav>
-      )}
-    </GlobalContext.Consumer>
+    <StyledNav>
+      {links.map((link) => (
+        <StyledNavLink
+          key={link.id}
+          $isActive={link.url === id}
+          onClick={() => {
+            closeDrawer();
+            setId(link.url);
+            updateNavLink({ url: link.url });
+          }}
+        >
+          {link.name}
+        </StyledNavLink>
+      ))}
+    </StyledNav>
+
   );
 }
 
